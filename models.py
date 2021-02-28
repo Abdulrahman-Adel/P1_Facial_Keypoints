184213ddd7112d929ef117015cd6ab5055e8e442
Abdulrahman-Adel <57441828+Abdulrahman-Adel@users.noreply.github.com>
2021-02-28 15:39:27 +0200

    Add files via upload

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -20,18 +19,43 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        
+        #Defining 1st convolutional layer
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        #Defining 2nd convolutional layer
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        #Defining Linear layer
+        self.linear_layer = nn.Sequential(
+            nn.Linear(186624, 1024),
+            nn.ReLU(inplace = True),
+            nn.Linear(1024, 136)
+        )
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
-
-        
+   
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.layer1(x)
+        x = self.layer2(x)
         
+        x = x.view(x.size(0), -1)
+        #print(x.size()) --> [1, 179776],or [1, 186624](with padding)
         
+        x = self.linear_layer(x)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
